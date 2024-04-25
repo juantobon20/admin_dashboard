7034657e9512762c1b5d276880cbc7b2994ed3b7
Juan Tobon <juantobon20@outlook.es>
2024-04-25 11:10:50 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Admin Dashboard',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
